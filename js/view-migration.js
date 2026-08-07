576196957ac90aaa18ee5c0e2f8503a91c535ca0
Stefan Filly <stefan.filly@gmx.de>
2026-08-07 11:01:23 +0200

    Cleanup phase 1: remove obsolete view migration

--- js/view-migration.js
+++
@@ -1,8 +0,0 @@
-/* Kompatibilität für gespeicherte Ansichten aus älteren Versionen. */
-'use strict';
-
-const loadBeforeViewMigration = load;
-load = function loadWithViewMigration() {
-  loadBeforeViewMigration();
-  if (S.view === 'einstellungen') S.view = 'uebersicht';
-};
